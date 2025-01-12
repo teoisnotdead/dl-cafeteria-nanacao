81ddcfc8ea7559fdfbc503b0f04f650a3bd7337e
TEO <alfredo.ssm@gmail.com>
2025-01-12 15:34:09 -0300

    Requerimientos 3 y 4

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -17,4 +17,18 @@ describe("Operaciones CRUD de cafes", () => {
     expect(response.statusCode).toBe(404);
     expect(response.body).toHaveProperty("message", "No se encontró ningún cafe con ese id");
   });
+
+  test("POST /cafes agrega un nuevo cafe y devuelve un status code 201", async () => {
+    const newCafe = { id: 5, nombre: "Latte" };
+    const response = await request(server).post("/cafes").send(newCafe);
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toContainEqual(newCafe);
+  });
+
+  test("PUT /cafes/:id devuelve 400 si el id del payload y los parametros no coinciden", async () => {
+    const updatedCafe = { id: 6, nombre: "Espresso" };
+    const response = await request(server).put("/cafes/7").send(updatedCafe);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("message", "El id del parámetro no coincide con el id del café recibido");
+  });
 });
